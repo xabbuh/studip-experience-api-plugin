dd39e82563eac16d15ff1d6ae50c34985f1684bc
Christian Flothmann <christian.flothmann@xabbuh.de>
2015-04-22 21:31:34 +0200

    rename api controller to lrs
    Stud.IP 3.1 treats routes containing /api in a special way. Thus,
    denying requests to the old API controller.

--- controllers/lrs.php
+++ controllers/lrs.php
@@ -24,7 +24,7 @@ use Xabbuh\XApi\Serializer\StatementSerializer;
  *
  * @property string $data
  */
-class ApiController extends StudipController
+class LrsController extends StudipController
 {
     /**
      * @var SerializerRegistry
